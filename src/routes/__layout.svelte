adeffcb9105280dc0c59b55d1d52e850d9f73fe9
오진수 <ohjinsu@codersproduct.com>
2022-03-26 16:00:41 +0900

    Fix the path of header

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Footer from '$lib/components/footer.svelte';
-	import Header from '$lib/components/Header.svelte';
+	import Header from '$lib/components/header.svelte';
 	import Main from '$lib/components/main.svelte';
 	import '../app.css';
 </script>
