746bf39ad24522155a2a697892809136ddb0bd37
오진수 <ohjinsu@codersproduct.com>
2022-04-01 00:37:58 +0900

    Add meta tags

--- src/lib/components/meta.svelte
+++ src/lib/components/meta.svelte
@@ -8,4 +8,10 @@
 <svelte:head>
   <title>{ formattedTitle }</title>
   <meta name="description" content={description} />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content={formattedTitle} />
+  <meta property="og:description" content={description} />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:title" content={formattedTitle} />
+  <meta name="twitter:description" content={description} />
 </svelte:head>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,10 @@
   // @ts-nocheck
   import Readme from "../../README.md"
   import Article from "$lib/layouts/article.svelte";
+import Meta from "$lib/components/meta.svelte";
 </script>
+
+<Meta description="오진수의 블로그입니다." />
 
 <Article>
   <Readme />
