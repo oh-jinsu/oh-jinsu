6ed57779ebe364ed072c2fdb9cfc62e2fcb3653e
오진수 <ohjinsu@codersproduct.com>
2022-04-01 15:42:55 +0900

    Add an error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        error,
+        status,
+      }
+    }
+  }
+</script>
+
+<script>
+  export let error
+
+  export let status
+
+  const message = status === 404 ? "페이지를 찾지 못했습니다." : "알 수 없는 오류입니다."
+</script>
+
+<h1>{ message }</h1>
