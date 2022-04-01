1e4d2d871208d407ad8c29b67b886877087efa35
오진수 <ohjinsu@codersproduct.com>
2022-04-01 14:16:44 +0900

    Remove redundant meta tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,7 @@
   // @ts-nocheck
   import Readme from "../../README.md"
   import Article from "$lib/layouts/article.svelte";
-import Meta from "$lib/components/meta.svelte";
 </script>
-
-<Meta description="오진수의 블로그입니다." />
 
 <Article>
   <Readme />
