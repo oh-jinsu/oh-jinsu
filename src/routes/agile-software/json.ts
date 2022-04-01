b4734c2a3f5dda43872829227093bdf072d61c5b
오진수 <ohjinsu@codersproduct.com>
2022-04-01 15:58:43 +0900

    Fix async fetching articles

--- src/routes/agile-software/json.ts
+++ src/routes/agile-software/json.ts
@@ -3,7 +3,7 @@ import { extractArticles } from "$lib/utils/article"
 export const get = async() => {
   const files = import.meta.glob("./*.md")
 
-  const result = extractArticles("agile-software", files)
+  const result = await extractArticles("agile-software", files)
 
   return {
     body: result,
--- src/routes/walking-logs/json.ts
+++ src/routes/walking-logs/json.ts
@@ -3,8 +3,8 @@ import { extractArticles } from "$lib/utils/article"
 export const get = async() => {
   const files = import.meta.glob("./*.md")
 
-  const result = extractArticles("agile-software", files)
-
+  const result = await extractArticles("agile-software", files)
+  
   return {
     body: result,
   }
