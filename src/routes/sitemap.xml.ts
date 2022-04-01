4358e8b22508a8c85850e6b5947c8ebc9a43b299
오진수 <ohjinsu@codersproduct.com>
2022-04-01 16:14:38 +0900

    Add a sitemap

---
+++ src/routes/sitemap.xml.ts
@@ -0,0 +1,35 @@
+export async function get() {
+  const [walkingLogs, agileSoftwares] = await Promise.all([
+    fetch("http://localhost:3000/walking-logs/json").then((res) => res.json()),
+    fetch("https://ohjinsu.me/agile-software/json").then((res) => res.json()),
+  ])
+
+  const articles = [...walkingLogs, ...agileSoftwares]
+
+  return {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+    body: `
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <urlset
+      xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xhtml="https://www.w3.org/1999/xhtml"
+      xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+      xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+      xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+      xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+    >
+      <url>
+        <loc>https://ohjinsu.me</loc>
+      </url>
+      ${articles.map(({href, date}) => `
+        <url>
+          <loc>https://ohjinsu.me/${href}</loc>
+          ${date ? `<lastmod>${date}</lastmod>` : ""}
+        </url>
+      `.trim()).join("")}
+    </urlset>
+    `.trim()
+  }
+}
--- src/routes/walking-logs/json.ts
+++ src/routes/walking-logs/json.ts
@@ -3,7 +3,7 @@ import { extractArticles } from "$lib/utils/article"
 export const get = async() => {
   const files = import.meta.glob("./*.md")
 
-  const result = await extractArticles("agile-software", files)
+  const result = await extractArticles("walking-logs", files)
   
   return {
     body: result,
