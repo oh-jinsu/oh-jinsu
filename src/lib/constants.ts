55b191fb1c76495b6287fc47c4ce5f9ec0cb13c1
오진수 <ohjinsu@codersproduct.com>
2022-07-26 13:16:22 +0900

    Update the profile

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,10 @@
 export const TITLE = "오진수"
-export const DESCRIPTION = "생산성을 중시하는 풀스택 개발자입니다."
+export const DESCRIPTION = "비즈니스 관점에서 사고하는 개발자에요."
 export const AUTHOR="오진수"
 export const AGILE_SOFTWARE = "애자일 소프트웨어"
 export const AGILE_SOFTWARE_DESCRIPTION="비즈니스의 성공을 주도하는 프로그래밍에 대해 생각해요."
 export const STARTUP_JOURNEY = "스타트업 여정"
-export const STARTUP_JOURNEY_DESCRIPTION = "사람들이 문제를 해결할 수 있도록 돕고 싶어요."
+export const STARTUP_JOURNEY_DESCRIPTION = "더 나은 세상을 만드는 일에 힘을 보태고 싶어요."
 export const URLS = {
   "애자일 소프트웨어": "agile-software",
   "스타트업 여정": "startup-journey"
